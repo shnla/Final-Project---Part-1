2cdcc0aa88078c13cb59f48d7b8f53539c0202cc
Shelena Zhang <shelena.yx.zhang@gmail.com>
2023-11-21 16:43:11 -0500

    solutions to worksheet input

--- index.ts
+++ index.ts
@@ -9,8 +9,23 @@ function printTotalPrice(): void{
     printValue.textContent = itemPrice.toFixed(2);
 
 }
-
 // addEventListener method takes in (type, listener)
 // type: an event type to listen for
 // listener: function that will respond to the event
 printBtn.addEventListener("click", printTotalPrice);
+
+const firstNumber = document.getElementById("number1") as HTMLInputElement;
+const secondNumber = document.getElementById("number2") as HTMLInputElement;
+const calculateBtn = document.getElementById("calculateButton") as HTMLButtonElement;
+const outputSum = document.getElementById("sum") as HTMLOutputElement;
+const outputDifference = document.getElementById("difference") as HTMLOutputElement;
+
+function printOutputs(): void{
+    const theSum = parseFloat(firstNumber.value) + parseFloat(secondNumber.value);
+    const theDifference = parseFloat(secondNumber.value) - parseFloat(firstNumber.value);
+    outputSum.textContent = theSum.toString();
+    outputDifference.textContent = theDifference.toString();
+
+}
+
+calculateBtn.addEventListener("click", printOutputs);
