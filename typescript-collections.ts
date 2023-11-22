f001fba043431ce213e34c26c2189d3b4353907d
shnla <150702602+shnla@users.noreply.github.com>
2023-11-22 03:36:20 +0000

    typescript collection notes finished

---
+++ typescript-collections.ts
@@ -0,0 +1,77 @@
+// TypeScript Collections
+// Working with larger sets of data using only one variable to store the collection of data
+// that variable is known as an array
+
+// TypeScript Collection Types
+
+// INDEXED COLLECTIONS
+// data set ordered by indexes and can be accessed by the index
+// an index: position of data. We always start counting index at 0.
+
+// Array: denoted by []. Can be a mix of different data types like numbers and strings. Values are separated with a comma. Access individual data by using its index.
+let arr = [1,2,'hello'];
+console.log(arr[0]); // Access: arrayName[index#]
+// Unlike Python, TypeScript does not have a built-in function to access values from the back of the array. You would have to code a loop to do that.
+// Output: 1
+
+// Array Methods
+// Arrays are mutable, meaning that its elements can be changed: deleted, added, moved, etc.
+console.log(arr.length) // .length returns the length of the array.
+const lastElement = arr.pop(); // pop function returns and removes the last element in an array
+const firstElement = arr.shift(); // shift function returns and removes the first element in an array
+delete arr[/*index*/]; // delete will remove an element in an array by its index
+arr.push(32,1); // adds a value to the end of the array. can push multiple values in one line
+// arr.length = 3
+// lastElement = 'hello' -> arr = [1,2]
+// firstElement = 1 -> arr = [2]
+// arr.push(32,1) -> arr = [2, 32, 1]
+
+// indexOf method returns the index of a given value
+const arr = ["walk the dog", "go shopping", "exercise"];
+const index = arr.indexOf("walk the dog") // arrayName.indexOf(value)
+// Output: 0
+// You can use the index to reassign its value using the Splice method.
+
+// Splice method: reassign a new value/delete an existing element. Needs the element index.
+const index = 2; // "exercise"
+const value = "go to the pharmacy";
+arr.splice(index, 1, value); // delete '1' element at 'index' (exercise), adds 'value' (go to pharmacy) to the right of 'index'
+// splice(index of element, number of elements to delete to the right (including the current element index), value to insert right of index)
+console.log(arr); // "walk the dog", "go shopping", "go to the pharmacy"
+
+
+// KEYED COLLECTIONS
+// data set ordered by keys instead of indices. 
+// Two types of keyed collections: Map and Set
+
+// Map: equivalent of python dictionaries
+let myMap = new Map();
+myMap.set("key", "value"); 
+let mapValue = myMap.get("key"); // returns the value associated with the given key
+console.log(mapValue);
+// Output: value
+// Keys and values don't have to be string: they can be numbers, or other data types. You can mix and match the key data type and the value data type
+
+// Map Methods: a few examples. there exists many more.
+myMap.clear(); // Remove all existing key-value pairs
+myMap.delete('key'); // Deletes the given key pair and returns True if it exists. Returns false if it doesn't exist within the map
+iterator = myMap.entries(); // Returns an iterator that contains all key-value pairs. Can be iterated through using iterator.next().value
+keys = myMap.keys(); // Returns an iterator containing only the keys
+values = myMap.values(); // Returns an iterator containing only the values
+
+
+// Sets: similar to an array, but cannot add repetitive values
+let mySet = new Set();
+mySet.add("apple");
+mySet.add("orange");
+mySet.add("apple");
+// Output: { 'apple', 'orange' }
+
+// Set Methods: a few examples, there exists many more
+mySet.clear(); // clears all values in the set. trying to print this will give undefined
+mySet.size(); // returns the length of the set. Printing this after clearing will return 0
+mySet.delete("value"); // deletes in the set the "value" passed in
+mySet.entries(); // returns an iterable object similar to a map. each value's key is the same as its alue
+mySet.keys(); // returns an iterable object of all the values
+mySet.values(); // does the same as the above. basically the keys in a set are equal to its values
+mySet.has("something") // returns true if the "something" is in the set, false if not
